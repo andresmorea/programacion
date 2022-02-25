3548416fb9a010a393a534124109e10cfcfa2ec2
andresmorea <100390538+andresmorea@users.noreply.github.com>
2022-02-24 19:49:04 -0500

    Add files via upload

---
+++ lista4.py
@@ -0,0 +1,8 @@
+idiomas=[]
+idiomas.append(input("digite un idioma: "))
+idiomas.append(input("digite un idioma: "))
+idiomas.append(input("digite un idioma: "))
+idiomas.append(input("digite un idioma: "))
+idiomas.append(input("digite un idioma: "))
+idiomas.append(input("digite un idioma: "))
+print(idiomas)
